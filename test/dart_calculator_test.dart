a996711b3701d6c24bcda2a373931cdc44632a43
alanfacol <alanms.silva@unifacol.edu.br>
2024-03-17 00:22:33 -0300

    refactor: update test

--- test/dart_calculator_test.dart
+++ test/dart_calculator_test.dart
@@ -2,5 +2,8 @@ import 'package:dart_calculator/calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {});
+  test('Calculator.calc()', () {
+    Calculator calculator = Calculator('+', 1, 1);
+    expect(calculator.calc(), equals(2));
+  });
 }
